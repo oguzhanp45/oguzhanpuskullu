1fa32a17d59165bbc042464c77243270112376d3
oguzhanp45 <mlgteam38@gmail.com>
2024-10-21 13:37:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 //Local storage ile popupu'ı tek seferlik çalıştırabilirdim fakat ödev olduğu için bu şekilde bırakıyorum hocam.
-// Pop-up'ı göster
 window.onload = function() {
-    document.getElementById('welcome-popup').style.display = 'flex';
-};
+    document.getElementById('popup').style.display = 'flex';
+}
 
-// Kapatma butonu
-document.getElementById('close-btn').onclick = function() {
-    document.getElementById('welcome-popup').style.display = 'none';
-};
+// Kapatma butonuna tıklandığında pop-up'ı kapat
+document.getElementById('closePopup').onclick = function() {
+    document.getElementById('popup').style.display = 'none';
+}
 
-// Kapatma simgesi
-document.querySelector('.close-btn').onclick = function() {
-    document.getElementById('welcome-popup').style.display = 'none';
-};
+// Pop-up dışına tıklandığında kapat
+window.onclick = function(event) {
+    if (event.target == document.getElementById('popup')) {
+        document.getElementById('popup').style.display = 'none';
+    }
+}
