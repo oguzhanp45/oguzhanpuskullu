276e6e82a28cfabb80029dfd3599eb19cc8d39b4
oguzhanp45 <mlgteam38@gmail.com>
2024-10-21 13:00:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 //Local storage ile popupu'ı tek seferlik çalıştırabilirdim fakat ödev olduğu için bu şekilde bırakıyorum hocam.
-window.onload = function() {
-    document.getElementById('popup').style.display = 'flex';
-}
-document.getElementById('closePopup').onclick = function() {
+document.getElementById('closePopup').addEventListener('click', function() {
     document.getElementById('popup').style.display = 'none';
-}
-window.onclick = function(event) {
-    if (event.target == document.getElementById('popup')) {
-        document.getElementById('popup').style.display = 'none';
-    }
-}
+});
+window.onload = function() {
+setTimeout(function() {
+document.getElementById('popup').style.display = 'block';
+}, 1000); // 1 saniye sonra açılır
+};
