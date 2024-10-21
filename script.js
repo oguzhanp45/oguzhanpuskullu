21e0a7722f2faddb192eefc75805ad6cfcdb0598
oguzhanp45 <mlgteam38@gmail.com>
2024-10-21 12:38:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//Local storage ile popupu ı tek seferlik çalıştırabilirdim fakat ödev olduğu için bu şekilde bırakıyorum hocam.
+window.onload = function() {
+    document.getElementById('popup').style.display = 'flex';
+}
+document.getElementById('closePopup').onclick = function() {
+    document.getElementById('popup').style.display = 'none';
+}
+window.onclick = function(event) {
+    if (event.target == document.getElementById('popup')) {
+        document.getElementById('popup').style.display = 'none';
+    }
+}
